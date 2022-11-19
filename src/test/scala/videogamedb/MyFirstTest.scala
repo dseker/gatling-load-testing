293f2d8e6239010c8ec09ef780c9b7c30d502a75
Diyadin Seker <Diyadin.seker1@gmail.com>
2022-11-19 15:34:04 +0100

    Successfully running first test

--- src/test/scala/videogamedb/MyFirstTest.scala
+++ src/test/scala/videogamedb/MyFirstTest.scala
@@ -1,5 +1,20 @@
 package videogamedb
 
-class MyFirstTest {
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
 
+
+class MyFirstTest extends Simulation {
+
+  // 1 Http configuration
+  val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
+    .acceptHeader("application/json")
+
+  // 2 Scenario Configuration
+  val scn = scenario("My first test")
+    .exec(http("Get all games")
+      .get("/videogame"))
+  // 3 Load Scenario
+  setUp(scn.inject(atOnceUsers(1)))
+    .protocols(httpProtocol)
 }
