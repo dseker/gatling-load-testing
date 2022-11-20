e4f86e051ede44ed196c1e5bf96dfe11b84508ba
Diyadin Seker <Diyadin.seker1@gmail.com>
2022-11-20 21:17:53 +0100

    check response code

---
+++ src/test/scala/videogamedb/scriptfundamentals/CheckResponseCode.scala
@@ -0,0 +1,36 @@
+package videogamedb.scriptfundamentals
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+class CheckResponseCode extends Simulation {
+
+  //noinspection TypeAnnotation
+  val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
+    .acceptHeader("application/json")
+
+  //noinspection TypeAnnotation
+  // scenario 2
+  val scn = scenario("Video Game DB - 3 calls")
+    .exec(http("get all video games - 1st call")
+      .get("/videogame")
+      .check(status.is(200)))
+
+    .pause(5)
+
+    .exec(http("Get a specific game")
+      .get("/videogame/1")
+    .check(status.is(200 to 2010)))
+    .pause(1, 10)
+
+    .exec(http("Get all video games - 2nd call")
+      .get("/videogame")
+    .check(status.not(400 to 404)))
+    .pause(3000.milliseconds)
+
+  setUp(scn.inject(atOnceUsers(1)))
+    .protocols(httpProtocol)
+
+}
